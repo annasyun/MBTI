006885ae3a97038e4549650be3d608d036e3fdb6
annasyun <yunjbly2470@naver.com>
2023-06-29 12:16:33 +0900

    feat: loading img added

--- js/result.js
+++ js/result.js
@@ -1,3 +1,13 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
 // localStorage에서 maxDeveloperType 값 가져오기
 const maxDeveloperType = localStorage.getItem("maxDeveloperType");
 
